4728929c2b75bfec22f87f484428246be80a2c9e
SazzDS <45617699+SazzDS@users.noreply.github.com>
2020-03-22 01:26:00 -0600

    Update Server.js For CORS

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ const server = app.listen(port, () => {
 
 // Error Handling
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin",  '*');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Credentials", true);
     next(createError(404));
 });
 
